5416acb3a8f1c6cdcc6c842d392ec8af7ab8a3e3
Jennie Rose <jennie@sevvie.ltd>
2021-09-02 10:28:34 -0700

    remove carriage returns from docker entrypoint

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -3,8 +3,6 @@
 if [ $ENVIRONMENT == "production" ]; then
   NGINX_ADDITIONAL_CONFIG='access_log off;'
 else
-  NGINX_ADDITIONAL_CONFIG="access_log /var/log/nginx/access.log;
-                           access_log /var/log/nginx/cache.log cache_st;
-                           access_log /var/log/nginx/proxy.log upstream_logging;"
+  NGINX_ADDITIONAL_CONFIG="access_log /var/log/nginx/access.log; access_log /var/log/nginx/cache.log cache_st; access_log /var/log/nginx/proxy.log upstream_logging;"
 fi
 nginx -g $NGINX_ADDITIONAL_CONFIG
