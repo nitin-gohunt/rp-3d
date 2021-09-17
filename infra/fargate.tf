f84f4841a980459c4af2ca61afb54a9e8763f2e6
Jennie Rose <jen@gohunt.com>
2021-09-17 11:39:39 -0700

    test: can I remove the slash before the star

--- infra/fargate.tf
+++ infra/fargate.tf
@@ -69,7 +69,7 @@ module "fargate" {
         }
       ],
       conditions = [{
-        path_patterns = ["/arcgis/sharing/rest/portals/self/*", "/arcgis/sharing/rest/content/items/*"]
+        path_patterns = ["/arcgis/sharing/rest/portals/self*", "/arcgis/sharing/rest/content/items*"]
       }]
     }
   ]
